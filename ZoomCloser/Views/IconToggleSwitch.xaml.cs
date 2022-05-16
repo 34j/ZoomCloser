3248fd22aa4b332b4cddd2e3a74de2054a3d0f82
34j <55338215+34j@users.noreply.github.com>
2022-05-17 01:01:19 +0900

    Found that this.DataContext = this is not ok in UserControl because binding would not work properly when user uses it in their views. Fixed and refactored IconToggleSwitch.cs.

--- ZoomCloser/Views/IconToggleSwitch.xaml.cs
+++ ZoomCloser/Views/IconToggleSwitch.xaml.cs
@@ -29,72 +29,14 @@ namespace ZoomCloser.Views
         public PackIconBoxIconsKind OffKind { get; set; }
         public string OnText { get; set; } = "On";
         public string OffText { get; set; } = "Off";
-        [DependencyProperty(OnPropertyChanged = nameof(OnIsOnChanged), Options = FrameworkPropertyMetadataOptions.BindsTwoWayByDefault)]
+        [DependencyProperty(Options = FrameworkPropertyMetadataOptions.BindsTwoWayByDefault)]
         public bool IsOn { get; set; }
         public string Header { get; set; }
-        [DependencyProperty(OnPropertyChanged =nameof(OnCommandChanged))]
         public ICommand Command { get; set; }
-
-        private static void OnIsOnChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            /*
-            var control = d as IconToggleSwitch;
-            var command = control.Command;
-            Debug.WriteLine(control.Header + ": " + control.IsOn + command);
-            if (command != null && command.CanExecute(null))
-            {
-                command?.Execute(null);
-
-            }*/
-        }
-        private static void OnCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            Debug.WriteLine("command changed!");
-        }            
-
-        /*
-        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register("Command",
-            typeof(ICommand),
-            typeof(IconToggleSwitch), null);
-        public ICommand Command { get => (ICommand)GetValue(CommandProperty); set => SetValue(CommandProperty, value); }
-
-        private static void OnCommandChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
-        {
-            (obj as IconToggleSwitch).ToggleSwitch.Command = (ICommand)e.NewValue;
-        }
-        mainwindowviewmodel->this binding notworking
-        */
-
-        public ToggleSwitch ToggleSwitch { get; init; }
-
 
         public IconToggleSwitch()
         {
-
             InitializeComponent();
-            if (this.HasContent)
-            {
-                ToggleSwitch = (ToggleSwitch)this.Content;
-                //(this.Content as FrameworkElement).DataContext = this;this is fucking
-            }
-            else
-            {
-                throw new NotImplementedException();
-            }
-            //this.DataContext = this;
-
-
-
-            //IsOnProperty.
-            /*var on = this.OnContent as FrameworkElement;
-            var off = this.OffContent as FrameworkElement;
-            foreach (var content in new FrameworkElement[] {on, off })
-            {
-                if(content != null)
-                {
-                    content.DataContext = this;
-                }
-            }*/
         }
 
     }
