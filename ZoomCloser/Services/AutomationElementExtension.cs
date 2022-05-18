0a380827cb58adddb8ffb52f35e9b18ccb8a3018
34j <55338215+34j@users.noreply.github.com>
2022-05-18 19:09:41 +0900

    Strengthened AutomationElementExtension.IsAlive()

--- ZoomCloser/Services/AutomationElementExtension.cs
+++ ZoomCloser/Services/AutomationElementExtension.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Automation;
+﻿using System.Windows.Automation;
 
 namespace ZoomCloser.Services
 {
@@ -18,9 +12,16 @@ namespace ZoomCloser.Services
             }
             try
             {
-                string t = element.Current.Name;
+                // Some values does not throw Exceptions when they are not available. Therefore, we need to check more properties to be sure that the element is alive
+                var t = element.Current.IsEnabled;
+                var n = element.Current.Name;
+                //var p = element.Current.ProcessId;
             }
-            catch
+            catch (ElementNotAvailableException)
+            {
+                return false;
+            }
+            catch (ElementNotEnabledException)
             {
                 return false;
             }
